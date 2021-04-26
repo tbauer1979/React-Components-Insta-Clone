75fde7599e87e1297c0ea9b38c7befd27a6ad27e
tom bauer <you@example.comthomasbauer1008@gmail.com>
2021-04-26 15:02:50 -0400

    done

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -4,10 +4,14 @@ import './Comments.css';
 
 const Comments = props => {
   // 🔥 Make sure the parent of Comments is passing the right props!
-  const { comments } = props;
-
+  const { comments  } = props;
   return (
-    <div>
+    <div> 
+    
+      {comments .map(comment => {
+      return <Comment key = {comment.id} comment = {comment} />
+    }
+    )}
       {/* map through the comments prop and render a Comment for every piece of data */}
     </div>
   );
